6c8b94feb484848793431575fdf96733baaeba30
Hoang Nguyen <hoangncd3@gmail.com>
2024-02-04 00:02:41 +0700

    feat: add input component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,13 @@
-import NextImage from 'next/image'
-
-import { Image } from '@/components/image/Image'
+import { Input } from '@/components/input'
 
 export default function Home() {
   return (
     <main>
-      <Image
-        as={NextImage}
-        width={300}
-        height={200}
-        src="https://nextui-docs-v2.vercel.app/images/hero-card-complete.jpeg"
-        alt="NextUI hero Image"
+      <Input
+        type="password"
+        label="Email"
+        labelPlacement="outside"
+        placeholder="Email"
       />
     </main>
   )
---
+++ src/components/input/Input.tsx
@@ -0,0 +1,6 @@
+'use client'
+
+import { Input as InputNextUI } from '@nextui-org/input'
+import { extendVariants } from '@nextui-org/react'
+
+export const Input = extendVariants(InputNextUI, {})
