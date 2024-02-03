a3a7a0360d2f81870e2f1584f299068f7b34edbd
Hoang Nguyen <hoangncd3@gmail.com>
2024-02-03 23:03:32 +0700

    feat: add chip component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,9 @@
-import { Checkbox } from '@/components/checkbox'
-import { CheckboxGroup } from '@/components/checkbox-group'
+import { Chip } from '@/components/chip/Chip'
 
 export default function Home() {
   return (
     <main>
-      <CheckboxGroup
-        label="Select cities"
-        defaultValue={['buenos-aires', 'london']}
-      >
-        <Checkbox value="buenos-aires">Buenos Aires</Checkbox>
-        <Checkbox value="sydney">Sydney</Checkbox>
-        <Checkbox value="san-francisco">San Francisco</Checkbox>
-        <Checkbox value="london">London</Checkbox>
-        <Checkbox value="tokyo">Tokyo</Checkbox>
-      </CheckboxGroup>
+      <Chip>Chip</Chip>
     </main>
   )
 }
---
+++ src/components/chip/Chip.tsx
@@ -0,0 +1,6 @@
+'use client'
+
+import { Chip as ChipNextUI } from '@nextui-org/chip'
+import { extendVariants } from '@nextui-org/react'
+
+export const Chip = extendVariants(ChipNextUI, {})
