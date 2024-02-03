f4e9e2aa8535ee2298e533619b3501b2c883e29f
Hoang Nguyen <hoangncd3@gmail.com>
2024-02-03 13:25:09 +0700

    feat: add button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,9 @@
-import { Avatar } from '@/components/avatar'
-import { Badge } from '@/components/badge'
+import { Button } from '@/components/button'
 
 export default function Home() {
   return (
     <main>
-      <Badge content="5" color="primary">
-        <Avatar
-          radius="md"
-          size="lg"
-          src="https://i.pravatar.cc/300?u=a042581f4e29026709d"
-        />
-      </Badge>
+      <Button color="primary">Button</Button>
     </main>
   )
 }
---
+++ src/components/button/Button.tsx
@@ -0,0 +1,6 @@
+'use client'
+
+import { Button as ButtonNextUI } from '@nextui-org/button'
+import { extendVariants } from '@nextui-org/react'
+
+export const Button = extendVariants(ButtonNextUI, {})
