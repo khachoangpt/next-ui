ee6648a09b1cfd9d3bc966730aeb4c66ea8f13eb
Hoang Nguyen <hoangncd3@gmail.com>
2024-02-03 23:20:34 +0700

    feat: add divider component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,24 @@
-import { Code } from '@/components/code'
+import { Divider } from '@/components/divider'
 
 export default function Home() {
   return (
     <main>
-      <Code>npm install @nextui-org/react</Code>
+      <div className="max-w-md">
+        <div className="space-y-1">
+          <h4 className="text-medium font-medium">NextUI Components</h4>
+          <p className="text-small text-default-400">
+            Beautiful, fast and modern React UI library.
+          </p>
+        </div>
+        <Divider className="my-4" />
+        <div className="flex h-5 items-center space-x-4 text-small">
+          <div>Blog</div>
+          <Divider orientation="vertical" />
+          <div>Docs</div>
+          <Divider orientation="vertical" />
+          <div>Source</div>
+        </div>
+      </div>
     </main>
   )
 }
---
+++ src/components/divider/Divider.tsx
@@ -0,0 +1,4 @@
+import { Divider as DividerNextUI } from '@nextui-org/divider'
+import { extendVariants } from '@nextui-org/react'
+
+export const Divider = extendVariants(DividerNextUI, {})
