6b9cd91c1b654f90345d99813a4feeb6f373306d
Hoang Nguyen <hoangncd3@gmail.com>
2024-02-03 22:57:09 +0700

    feat: add checkbox group component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,19 @@
 import { Checkbox } from '@/components/checkbox'
+import { CheckboxGroup } from '@/components/checkbox-group'
 
 export default function Home() {
   return (
     <main>
-      <Checkbox defaultSelected>Option</Checkbox>
+      <CheckboxGroup
+        label="Select cities"
+        defaultValue={['buenos-aires', 'london']}
+      >
+        <Checkbox value="buenos-aires">Buenos Aires</Checkbox>
+        <Checkbox value="sydney">Sydney</Checkbox>
+        <Checkbox value="san-francisco">San Francisco</Checkbox>
+        <Checkbox value="london">London</Checkbox>
+        <Checkbox value="tokyo">Tokyo</Checkbox>
+      </CheckboxGroup>
     </main>
   )
 }
---
+++ src/components/checkbox-group/CheckboxGroup.tsx
@@ -0,0 +1,6 @@
+'use client'
+
+import { CheckboxGroup as CheckboxGroupNextUI } from '@nextui-org/checkbox'
+import { extendVariants } from '@nextui-org/react'
+
+export const CheckboxGroup = extendVariants(CheckboxGroupNextUI, {})
